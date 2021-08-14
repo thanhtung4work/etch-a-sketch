3511439d83fde8e4ddb52318fc2346906967a82a
Tran Thanh Tung <thanhtung.4work@gmail.com>
2021-08-14 14:51:47 +0700

    add rainbow and eraser

--- app.js
+++ app.js
@@ -2,8 +2,14 @@
 const grid = document.querySelector('.grid');
 const resetBtn = document.querySelector('#reset');
 const applyBtn = document.querySelector('#apply');
+const blackBtn = document.querySelector('#blackBtn');
+const eraserBtn = document.querySelector('#eraserBtn');
+const rainbowBtn = document.querySelector('#rainbowBtn');
 const input = document.querySelector('#input');
-const gridOut = document.querySelector('#gridOutput')
+const gridOut = document.querySelector('#gridOutput');
+
+
+let currentColor = 'black';
 let boxPerRow = 10;
 
 function createDiv(size){
@@ -33,15 +39,43 @@ grid.addEventListener('mouseover', function (e){
     //set active when hover on box
     if (e.target.classList.contains('box')){
         e.target.classList.add('active');
+
+        //draw color
+        if(currentColor == 'random'){
+            //generate random rgb color
+            let r = Math.floor( Math.random() * 256 );
+            let g = Math.floor( Math.random() * 256 );
+            let b = Math.floor( Math.random() * 256 );
+
+            e.target.style['background-color'] = `rgb(${r}, ${g}, ${b})`;
+        }else{
+            e.target.style['background-color'] = currentColor;
+        }
+            
         console.log('hover');
     }
 });
 
 resetBtn.addEventListener('click', resetGrid)
 applyBtn.addEventListener('click', function(e){
+    if (input.value == "") return;
     boxPerRow = input.value;
     gridOut.innerHTML = `${boxPerRow} x ${boxPerRow}`;
     resetGrid();
 })
 
+rainbowBtn.addEventListener('click', function (e){
+    currentColor = 'random';
+    document.querySelector('#status').innerHTML = 'rainbow';
+})
+
+blackBtn.addEventListener('click', function (e){
+    currentColor = 'black';
+    document.querySelector('#status').innerHTML = 'black';
+})
+
+eraserBtn.addEventListener('click', function(e){
+    currentColor = 'white';
+    document.querySelector('#status').innerHTML = 'eraser';
+})
 createGrid();
